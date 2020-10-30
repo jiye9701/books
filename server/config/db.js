090e0d6cc827f94a5205437cd865be23fb7bd47a
jiye9701 <60005378+jiye9701@users.noreply.github.com>
2020-10-30 07:44:52 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  "URI": "mongodb://localhost/book_store"
+
+  
+};
+
+//local MongoDB deployment ->
+
+//module.exports.URL = "mongodb+srv://jiye:<wjpYAzGhxuVvwMVc>@mongodbserver.spi3d.mongodb.net/<book_store>?retryWrites=true&w=majority"
+
+//const MongoClient = require('mongodb').MongoClient;
+//const uri = "mongodb+srv://jiye:<wjpYAzGhxuVvwMVc>@mongodbserver.spi3d.mongodb.net/<book_store>?retryWrites=true&w=majority";
+//const client = new MongoClient(uri, { useNewUrlParser: true });
+//client.connect(err => {
+  //const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  //client.close();
+//});
